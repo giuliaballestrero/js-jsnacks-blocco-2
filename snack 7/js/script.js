4ecd918c7b0b7783b4112f3a272e28ee86b21fbb
giuliaballestrero <giulia.ballestrero@gmail.com>
2022-11-28 15:12:18 +0100

    Snack 7

---
+++ snack 7/js/script.js
@@ -0,0 +1,34 @@
+
+// Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente con un ciclo while.
+ 
+//creo una lista della spesa
+const shoppingList = [ 
+    'pane',
+    'latte',
+    'uova',
+    'rosmarino',
+    'paprika',
+    'guanciale',
+    'vino'
+]
+
+//indico l'elemento del DOM che voglio andare a modificare
+const ulElement = document.querySelector('ul.list-group');
+let i= -1;
+
+//scorro gli elementi della lista fino alla fine
+while (i < 6) {
+    i++;
+    //creo un list item
+    const newLiElement = document.createElement('li');
+
+    //assegno una classe al mio list item
+    newLiElement.classList.add('list-group-item');
+
+    //dico al mio list item di avere come contenuto un indice della lista della spesa
+    newLiElement.innerText = shoppingList[i];
+
+    //appendo l'elemento list item alla ul del DOM
+    ulElement.append(newLiElement);
+
+};
